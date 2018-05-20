b30de4f4a68ca66641d342c964fc5df7c2ed38bf
Ben Pardo <visaudi@gmail.com>
2018-05-19 23:23:10 -0600

    Logs with parseq.

--- index.js
+++ index.js
@@ -5,12 +5,22 @@
 /*global
     console
 */
-function logInvite(event) {
+
+import parseq from "./parseq.js";
+
+function widgetRequestor(callback, success) {
     "use strict";
-    console.log(JSON.parse(event.target.responseText));
+    const invitationRequest = new XMLHttpRequest();
+    invitationRequest.addEventListener("load", function successHandler(event) {
+        const discordInformation = JSON.parse(event.target.responseText);
+        callback(discordInformation); 
+    });
+    invitationRequest.open("GET", "https://discordapp.com/api/guilds/430758941530783754/widget.json");
+    invitationRequest.send();
 }
 
-const invitationRequest = new XMLHttpRequest();
-invitationRequest.addEventListener("load", logInvite);
-invitationRequest.open("GET", "https://discordapp.com/api");
-invitationRequest.send();
+function log(value, reason) {
+    console.log(value);
+}
+
+parseq.sequence([widgetRequestor])(log);
